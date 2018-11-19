68b49230e8a272fb2ce6005ff65b1b4a7e7a4222
Carl Johan Freme <freme@kth.se>
2018-11-19 16:19:04 +0100

    display setup

--- main.c
+++ main.c
@@ -1,6 +1,55 @@
 #include <pic32mx.h>
+#include "display.h"
 
+void setup(void);
+void game_loop(void);
 
-int main() {
+int main(void) {
+	setup();
+	game_loop();
+
 	return 0;
+}
+
+void setup(void){
+	//Kanske behövs
+	/* Set up peripheral bus clock */
+	OSCCON &= ~0x180000;
+	OSCCON |= 0x080000;
+
+	/* Set up output pins */
+	AD1PCFG = 0xFFFF;
+	ODCE = 0x0;
+	TRISECLR = 0xFF;
+	PORTE = 0x0;
+
+	/* Output pins for display signals */
+	PORTF = 0xFFFF;
+	PORTG = (1 << 9);
+	ODCF = 0x0;
+	ODCG = 0x0;
+	TRISFCLR = 0x70;
+	TRISGCLR = 0x200;
+
+	//Kanske behövs
+	/* Set up input pins */
+	TRISDSET = (1 << 8);
+	TRISFSET = (1 << 1);
+
+	/* Set up SPI as master */
+	SPI2CON = 0;
+	SPI2BRG = 4;
+
+	/* Clear SPIROV*/
+	SPI2STATCLR &= ~0x40;
+	/* Set CKP = 1, MSTEN = 1; */
+    SPI2CON |= 0x60;
+	/* Turn on SPI */
+	SPI2CONSET = 0x8000;
+	init();
+}
+
+void game_loop(){
+	display_string(2,"Hej pa dig");
+	update();
 }
